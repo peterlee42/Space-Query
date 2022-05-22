0d5d417007d655bfc01263e2c67ffbd0fc3c0f75
pjax42 <76502486+pjax42@users.noreply.github.com>
2022-05-21 20:54:08 -0400

    Added buttons and input box

--- main.py
+++ main.py
@@ -1,33 +1,96 @@
 import pygame
 from button import Button
-from colours import *
 
 pygame.init()
 
+# Button colours: Green 15d798,
+# VARIABLES
 WIDTH, HEIGHT = 1280, 720
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
 
 
-def main_menu():
-    mainMenuBG = pygame.image.load("assets/spaceBG.jpeg")
-    testButton = pygame.image.load("assets/button_test.png").convert_alpha()
+def main():
+    # LOAD IMGS
+    background = pygame.image.load("assets/spaceBG.jpeg")
+    btnSpaceQuery = pygame.image.load(
+        "assets/btnSpaceQuery.png").convert_alpha()
+    btnQuit = pygame.image.load("assets/btnQuit.png").convert_alpha()
 
-    pygame.display.set_caption("Menu")
+    # TITLE
+    font = pygame.font.SysFont("corbel", 60)
+    title = font.render("Welcome to Space Query!", True, GREEN, BLUE)
+    titleRect = title.get_rect()
+    titleRect.center = (WIDTH//2, HEIGHT//8)
 
-    test_button = Button(100, 200, testButton, 0.8)
+    # BUTTONS
+    btnSpaceQuery = Button(WIDTH/4, HEIGHT/3*2, btnSpaceQuery, 0.8)
+    btnQuit = Button(WIDTH/5*3, HEIGHT/3*2, btnQuit, 0.8)
 
+    # TEXT INPUT
+    baseFont = pygame.font.Font(None, 40)
+    userText = ''
+    inputRect = pygame.Rect(WIDTH//8, HEIGHT//4, 600, 50)
+    activeColour = pygame.Color("cadetblue1")
+    passiveColour = pygame.Color("blueviolet")
+    colour = passiveColour
+    active = False
+
+    # DISPLAY
+    pygame.display.set_caption("Main Menu")
+    clock = pygame.time.Clock()
     run = True
     while run:
-        SCREEN.blit(mainMenuBG, (0, 0))
+        # Sets FPS to 60. Adds background and title to screen.
+        clock.tick(60)
+        SCREEN.blit(background, (0, 0))
+        SCREEN.blit(title, titleRect)
 
-        if test_button.draw(SCREEN):
+        # Adds buttons to screen and checks if it is pressed every frame
+        if btnSpaceQuery.draw(SCREEN):
             print("PRESSED")
+        if btnQuit.draw(SCREEN):
+            run = False
 
+        # Puts input text box on display
+        if active:
+            colour = activeColour
+        else:
+            colour = passiveColour
+        pygame.draw.rect(SCREEN, colour, inputRect)
+        textSurface = baseFont.render(userText, True, (255, 255, 255))
+        SCREEN.blit(textSurface, (inputRect.x+5, inputRect.y+5))
+        inputRect.w = max(600, textSurface.get_width()+10)
+        pygame.display.flip()
+
+        # Quits program if application is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
+            # Input text of query
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if inputRect.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
+
+            if event.type == pygame.KEYDOWN:
+                if active == True:
+                    if event.key == pygame.K_BACKSPACE:
+                        userText = userText[:-1]
+                    elif event.key == pygame.K_RETURN:
+                        print(userText)
+                    else:
+                        userText += event.unicode
+
         pygame.display.update()
 
 
-main_menu()
+def spaceQuery():
+    return
+
+
+main()
