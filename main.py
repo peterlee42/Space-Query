cadc460470771737abddf1bd34127782f10bcbf7
pjax42 <76502486+pjax42@users.noreply.github.com>
2022-05-21 23:25:33 -0400

    Added second page and buttons

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
+import sys
 from button import Button
 from query import answerQuery as aq
 
 pygame.init()
 
-# Button colours: Green 15d798,
 # VARIABLES
 WIDTH, HEIGHT = 1280, 720
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 128)
+BLACK = (0, 0, 0)
 
 
 def main():
     # LOAD IMGS
     background = pygame.image.load("assets/spaceBG.jpeg")
-    btnSpaceQuery = pygame.image.load(
-        "assets/btnSpaceQuery.png").convert_alpha()
+    btnSearch = pygame.image.load(
+        "assets/btnSearch.png").convert_alpha()
+    btnSearchVoice = pygame.image.load(
+        "assets/btnSearchVoice.png").convert_alpha()
+    btnSolarSystem = pygame.image.load(
+        "assets/btnSolarSystem.png").convert_alpha()
+    btnTrackISS = pygame.image.load("assets/btnTrackISS.png")
     btnQuit = pygame.image.load("assets/btnQuit.png").convert_alpha()
 
     # TITLE
@@ -27,14 +33,15 @@ def main():
     titleRect.center = (WIDTH//2, HEIGHT//8)
 
     # BUTTONS
-    btnSpaceQuery = Button(WIDTH//4*3, HEIGHT//8*2, btnSpaceQuery, 0.8)
-    btnQuit = Button(WIDTH/5*3, HEIGHT/3*2, btnQuit, 0.8)
+    btnSearch = Button(WIDTH//4*3, HEIGHT//4, btnSearch, 0.8)
+    btnQuit = Button(WIDTH//4*3, HEIGHT//8*7, btnQuit, 0.8)
+    btnSearchVoice = Button(WIDTH//4*3, HEIGHT//8*3, btnSearchVoice, 0.8)
 
     # TEXT INPUT
     baseFont = pygame.font.Font(None, 40)
     userText = ""
     previousLetters = 0
-    inputRect = pygame.Rect(WIDTH//10, HEIGHT//8*2, 600, 50)
+    inputRect = pygame.Rect(WIDTH//10, HEIGHT//8*2, 800, 50)
     activeColour = pygame.Color("cadetblue1")
     passiveColour = pygame.Color("blueviolet")
     colour = passiveColour
@@ -42,21 +49,26 @@ def main():
 
     # DISPLAY
     pygame.display.set_caption("Main Menu")
+
+    SCREEN.fill(BLACK)
+
     clock = pygame.time.Clock()
-    run = True
-    while run:
+    while True:
         # Sets FPS to 60. Adds background and title to screen.
         clock.tick(60)
         SCREEN.blit(background, (0, 0))
         SCREEN.blit(title, titleRect)
 
-        # Adds buttons to screen and checks if it is pressed every frame
-        if btnSpaceQuery.draw(SCREEN):
-            print("PRESSED")
+        # DISPLAY BUTTONS AND CHECK IF IT HAS BEEN PRESSED
+        if btnSearch.draw(SCREEN):
+            spaceQuery()
+        if btnSearchVoice.draw(SCREEN):
+            searchVoice()
         if btnQuit.draw(SCREEN):
-            run = False
+            pygame.quit()
+            sys.exit()
 
-        # Puts input text box on display
+        # DISPLAY AND TYPE IN TEXT INPUT
         if active:
             colour = activeColour
         else:
@@ -65,14 +77,13 @@ def main():
         textSurface = baseFont.render(
             userText[previousLetters:], True, (255, 255, 255))
         SCREEN.blit(textSurface, (inputRect.x+5, inputRect.y+5))
-        inputRect.w = max(800, textSurface.get_width()+10)
         pygame.display.flip()
 
         # Quits program if application is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-
+                pygame.quit()
+                sys.exit()
             # Input text of query
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if inputRect.collidepoint(event.pos):
@@ -103,6 +114,50 @@ def main():
 
 
 def spaceQuery():
+    # LOAD IMGS
+    background = pygame.image.load("assets/spaceBG.jpeg")
+    btnQuit = pygame.image.load("assets/btnQuit.png").convert_alpha()
+    btnBack = pygame.image.load("assets/btnBack.png").convert_alpha()
+
+    # TITLE
+    font = pygame.font.SysFont("corbel", 60)
+    title = font.render("To Answer Your Question...", True, GREEN, BLUE)
+    titleRect = title.get_rect()
+    titleRect.center = (WIDTH//2, HEIGHT//8)
+
+    # BUTTONS
+    btnQuit = Button((WIDTH//4*3), HEIGHT/8*7, btnQuit, 0.8)
+    btnBack = Button(WIDTH//10, HEIGHT/8*7, btnBack, 0.8)
+
+    # DISPLAY
+    pygame.display.set_caption("Query")
+
+    SCREEN.fill(BLACK)
+
+    clock = pygame.time.Clock()
+    while True:
+        # Sets FPS to 60. Adds background and title to screen.
+        clock.tick(60)
+        SCREEN.blit(background, (0, 0))
+        SCREEN.blit(title, titleRect)
+
+        # Adds buttons to screen and checks if it is pressed every frame
+        if btnBack.draw(SCREEN):
+            main()
+        if btnQuit.draw(SCREEN):
+            pygame.quit()
+            sys.exit()
+
+        # Quits program if application is closed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+
+def searchVoice():
     return
 
 
